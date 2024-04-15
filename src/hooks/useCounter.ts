898b2d746ec3212e4618d5aa38493b98c8fb1019
93mcqmil <mcqueen_1993@hotmail.com>
2024-04-15 14:50:38 +0200

    task 7

--- src/hooks/useCounter.ts
+++ src/hooks/useCounter.ts
@@ -1,26 +1,52 @@
 import { useState } from "react";
 
 function useCounter(initialvalue: number, maxCount: number, minCount: number) {
-    const [count, setCount] = useState<number>(initialvalue) // state to keep track of Count
+    const [count, setCount] = useState<number>(initialvalue) // state to track count
+    const [isEven, setIsEven] = useState(initialvalue % 2 === 0)
 
 
-    // function to increment/decrement count
+
+
+    // function to increment no further than MAX
     const increment = () => {
         if (count < maxCount) {
-            setCount(count + 1)
+            setCount(count => count + 1)
+            setIsEven((count + 1) % 2 === 0)
 
         } else {
             alert("Max value achieved")
         }
     }
 
+    // function to decrement no further than MIN
     const decrement = () => {
         if (count > minCount) {
-            setCount(count - 1)
+            setCount(count => count - 1)
+            setIsEven((count - 1) % 2 === 0)
         } else {
             alert("Min value achieved")
         }
 
+    }
+
+    const add10 = () => {
+        if (count > maxCount) {
+            setCount(count + 10)
+
+        } else {
+            alert("Max value achieved")
+        }
+
+    }
+
+    const min10 = () => {
+        if (count < minCount) {
+            setCount(count - 10)
+
+
+        } else {
+            alert("Min value achieved")
+        }
     }
     const reset = () => setCount(initialvalue)
 
@@ -28,6 +54,6 @@ function useCounter(initialvalue: number, maxCount: number, minCount: number) {
 
 
     // Returns count and increment function
-    return { count, setCount, increment, decrement, reset, maxCount, minCount }
+    return { count, isEven, setCount, increment, add10, min10, decrement, reset, maxCount, minCount }
 }
 export default useCounter;
